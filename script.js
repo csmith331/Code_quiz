d47d5816735cdfedbf6e6b5905171631bf71f0a6
Chris Smith <csmith331@outlook.com>
2021-04-21 13:52:32 -0400

    redid my CSS, HTML and Javascript section due to running into hidden and visible issues in Javascript

--- script.js
+++ script.js
@@ -1,95 +1,115 @@
 var NavHighscores = document.querySelector(".View-Highscores");
 var NavTime = document.querySelector(".Time");
-var Container0 = document.querySelector(".container0");
-var StartQuiz = document.querySelector("start-quiz");
-var Container1 = document.querySelector("container1");
-var CorrectAnswer = document.querySelector(".correct");
-var WrongAnswer = document.querySelector(".wrong");
-var Container2 = document.querySelector(".container2");
-var Container3 = document.querySelector(".container3");
-var Container4 = document.querySelector(".container4");
-var Container5 = document.querySelector(".container5");
-var Container6 = document.querySelector(".container6");
-var SubmitButton = document.querySelector(".submit-button");
-var Container7 = document.querySelector("container7");
-var GoBackButton = document.querySelector(".Go-back-button");
-var ClearButton = document.querySelector(".clear-button");
+var Container0 = document.querySelector(".container");
+var StartQuiz = document.querySelector(".start-quiz");
+var Answers = document.querySelector(".answers");
+var Choices = document.querySelector("question-color");
 
-var Container1 = [
-  { 
-    question : "Commonly used Data Types do not include:",
-    options : ["Strings", "Booleans", "Alerts", "Numbers"],  
-    answer : 2,
-    hidden = false,
-    visible = true,
-  }
+
+
+var questionIndex = 0;
+
+var counter = 0
+
+var array = [
+
+    {
+
+        questions:"Commonly used Data Types do not include:",
+
+        choices:["String", "Booleans", "Alerts", "Numbers"],
+
+        answers: "Booleans",
+
+    }
+
+    ,{
+
+        questions:"The condition in an if/else statement is enclosed within__________.",
+
+        choices: ["Quotes", "Curly brackets", "Parenthesis", "Square brackets"],
+        
+        answers: "Curly brackets",
+
+    }
+
+    ,{
+
+        questions:"Arrays in Javascript can be used to store __________.",
+
+        choices: ["Numbers and strings", "Other arrays", "Booleans", "All the above"],
+
+        answers: "Booleans",
+
+    }
+
+    ,{
+        questions: "String values must be enclosed within _______ when being assigned to variables.",
+
+        choices: ["Commas", "Curly brackets", "Quotes", "Parenthesis"],
+
+        answers: "Curly brackets",
+    }
+
+    ,{
+        questions: "A very useful tool used during development and debugging for printing content to the debugger is:",
+
+        choices: ["Javascript", "Terminal/bash", "For loops", "Console.log"],
+
+        answers: "For loops",
+    }
+
 ]
 
-var Container2 = [
-  {
-    question : "The condition in an if/else statement is enclosed within _______.",
-    options : ["Quotes", "Curly brackets", "Parenthesis", "Square brackets"],
-    answer: 2,
-    hidden = false,
-    visible = true,
-  }
-]
 
-var Container3 = [
-  {
-    question : "Arrays in Javascript can be used to store ________.",
-    options : ["Numbers and strings", "Other arrays", "Booleans", "All the above"],
-    answer: 3,
-    hidden = false,
-    visible = true,
-  }
-]
+var secondsStart = 75;
+var Interval = 0;
+var timeDeduction = 15;
 
-var Container4 = [
-  {
-    question: "String values must be enclosed within ______ when being assigned to variables.",
-    options : ["Commas", "Curly brackets", "Quotes", "Parenthesis"],
-    answer : 2,
-    hidden = false,
-    visible = true,
-  }
-]
-
-var Container5 = [
-  {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    options : ["Javascript", "Terminal/bash", "For loops", "Console.log"],
-    answer: 3,
-    hidden = false,
-    visible = true,
-  }
-]
-
-var wrong;
-var correct;
-var highscores;
-var timer;
-var timecount;
+//need to create a timer function
 
 
-function startquiz() {
-    timerCount = 75;
-    // Prevents start button from being clicked when round is in progress
-    startButton.disabled = true;
-    startTimer()
-  }
 
-  function highscores() {
-    High.textContent = highscores;
-    localStorage.setItem("highscores", highscores);
-  }
+function render (questionIndex) {
+question
+}
 
-  function wrongAnswer() {
-    WrongAnswer.textContent = "Wrong";
-    
-  }
 
-  function correctAnswer() {
-    CorrectAnswer.textContent = "Correct";
-    
-  }
+
+
+// questionDiv.textContent = questions[counter]
+
+// answerDiv.textContent = answers[counter]
+
+
+
+// function selectAnswer(){
+
+//     //statement that compares their answer to the correct answer
+
+//     if(true){
+
+//         alert('you got the question right')
+
+//     }
+
+//     counter++
+
+//     questionDiv.innerHTML = array[2].question
+
+//     answerDiv.innerHTML = array[2].answer
+
+}
+
+// function start(){
+
+//     // start timer
+
+//     document.getElementById('start').style.visibility = 'hidden'
+
+//     document.getElementById('question').style.visibility = 'visible'
+
+//     document.getElementById('answer').style.visibility = 'visible'
+
+}
+
